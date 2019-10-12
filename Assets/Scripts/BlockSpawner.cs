3de0d347c8f32166d0d805e7dab23b587050d363
mcb3 <matheuscberger@gmail.com>
2019-10-11 23:25:05 -0300

    modified .gitignore, began core mechanic dev

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    [Range(1,5)]
+    public int spawnTimer = 2;
+
+    public GameObject obstacle;
+
+    private float elapsedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnTimer < elapsedTime)
+        {
+            Instantiate(obstacle, new Vector3(), this.transform.rotation);
+            elapsedTime = 0;
+        }
+
+        elapsedTime += Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement += new Vector3(-2, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            movement += new Vector3(0, 0, -2);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movement += new Vector3(2, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movement += new Vector3(0, 0, 2);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movement.Scale(new Vector3(18, 0, 10));
+        }
+
+        this.GetComponent<Rigidbody>().AddForce(movement, ForceMode.Impulse);
+    }
+}
